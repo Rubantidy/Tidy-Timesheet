9a59ec445b29138be13b90351b682fddabadaa5f
PracticeMust <practicemust1>
2025-02-11 18:32:12 +0530

    updated

--- TimeSheet/src/main/java/timesheet/admin/dao/Employeedao.java
+++ TimeSheet/src/main/java/timesheet/admin/dao/Employeedao.java
@@ -2,6 +2,7 @@ package timesheet.admin.dao;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
+@Getter
+@Setter
+@ToString
 @Table(name = "Employee")
 public class Employeedao {
 
@@ -32,56 +36,5 @@ public class Employeedao {
 
     @JsonProperty("E-role")
     private String eRole;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String geteName() {
-		return eName;
-	}
-
-	public void seteName(String eName) {
-		this.eName = eName;
-	}
-
-	public String geteMail() {
-		return eMail;
-	}
-
-	public void seteMail(String eMail) {
-		this.eMail = eMail;
-	}
-
-	public String getePass() {
-		return ePass;
-	}
-
-	public void setePass(String ePass) {
-		this.ePass = ePass;
-	}
-
-	public String geteRole() {
-		return eRole;
-	}
-
-	public void seteRole(String eRole) {
-		this.eRole = eRole;
-	}
-
-	@Override
-	public String toString() {
-		return "Employeedao [id=" + id + ", eName=" + eName + ", eMail=" + eMail + ", ePass=" + ePass + ", eRole="
-				+ eRole + "]";
-	}
-	
-    
-	
-	
-	
 
 }
